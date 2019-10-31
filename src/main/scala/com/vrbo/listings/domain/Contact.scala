7f8f1ab9b0c0f2dfd0c0265f4b3208f6cf479fbb
Zahir Abdi <zabdi@expediagroup.com>
2019-10-31 12:03:06 +0000

    Fix

--- src/main/scala/com/vrbo/listings/domain/Contact.scala
+++ src/main/scala/com/vrbo/listings/domain/Contact.scala
@@ -2,4 +2,4 @@ package com.vrbo.listings.domain
 
 import com.twitter.finatra.validation.Size
 
-case class Contact(@Size(min = 5, max = 12) phone: String, @Size(min = 5, max = 12)formattedPhone: String)
+case class Contact(@Size(min = 5, max = 12) phone: String, @Size(min = 5, max = 16)formattedPhone: String)
